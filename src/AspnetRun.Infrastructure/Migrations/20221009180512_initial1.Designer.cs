e916eb9325e7bec7439064c8a27d98dc46d5fdcf
Hamid Raza Khan <67641619+HamidRazaKhan78655@users.noreply.github.com>
2022-10-09 23:09:28 +0500

    [MIGRATION] Initial Migration

--- src/AspnetRun.Infrastructure/Migrations/20221009180512_initial1.Designer.cs
+++ src/AspnetRun.Infrastructure/Migrations/20221009180512_initial1.Designer.cs
@@ -4,6 +4,7 @@ using AspnetRun.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AspnetRun.Infrastructure.Migrations
 {
     [DbContext(typeof(AspnetRunContext))]
-    partial class AspnetRunContextModelSnapshot : ModelSnapshot
+    [Migration("20221009180512_initial1")]
+    partial class initial1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/AspnetRun.Infrastructure/Migrations/20221009180512_initial1.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AspnetRun.Infrastructure.Migrations
+{
+    public partial class initial1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropSequence(
+                name: "aspnetrun_type_hilo");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Product",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ProductReason",
+                table: "Product",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Category",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.CreateTable(
+                name: "Game",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    HighScore = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Game", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Game");
+
+            migrationBuilder.DropColumn(
+                name: "ProductReason",
+                table: "Product");
+
+            migrationBuilder.CreateSequence(
+                name: "aspnetrun_type_hilo",
+                incrementBy: 10);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Product",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Category",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+        }
+    }
+}
